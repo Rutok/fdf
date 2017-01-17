daf6ac25432257a8ec06e52749a9148db12521e9
Rutok <n.boste@gmail.com>
2017-01-17 05:02:13 +0100

    Event updt

--- srcs/fdf_event.c
+++ srcs/fdf_event.c
@@ -6,7 +6,7 @@
 /*   By: nboste <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 18:18:34 by nboste            #+#    #+#             */
-/*   Updated: 2017/01/17 02:43:06 by nboste           ###   ########.fr       */
+/*   Updated: 2017/01/17 05:00:45 by nboste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ static int	event_to_process(t_event *ev)
 {
 	if (ev->key_nav.keys[KEY_LEFT]
 		|| ev->key_nav.keys[KEY_RIGHT] || ev->key_nav.keys[KEY_UP]
-		|| ev->key_nav.keys[KEY_DOWN] || ev->key_pad.keys[KEY_KP_PLUS]
-		|| ev->key_pad.keys[KEY_KP_MINUS])
+		|| ev->key_nav.keys[KEY_DOWN] || ev->key_nav.keys[KEY_PAGEUP]
+		|| ev->key_nav.keys[KEY_PAGEDOWN])
 		return (1);
 	return (0);
 }
@@ -30,8 +30,8 @@ static void	reset_event(t_event *ev)
 	ev->key_nav.keys[KEY_RIGHT] = 0;
 	ev->key_nav.keys[KEY_UP] = 0;
 	ev->key_nav.keys[KEY_DOWN] = 0;
-	ev->key_pad.keys[KEY_KP_PLUS] = 0;
-	ev->key_pad.keys[KEY_KP_MINUS] = 0;
+	ev->key_nav.keys[KEY_PAGEUP] = 0;
+	ev->key_nav.keys[KEY_PAGEDOWN] = 0;
 }
 
 void	fdf_events(t_env *env)
@@ -43,6 +43,8 @@ void	fdf_events(t_env *env)
 	fdf = (t_fdf *)env->app.d;
 	while (ev->in_use) {SDL_Delay(2);}
 	ev->in_use = 1;
+	if (ev->key_special.keys[KEY_ESCAPE])
+		ev->exit = 1;
 	if (ev->key_nav.keys[KEY_LEFT])
 		fdf_apply_matrix(fdf->map, fdf->matrix.rot_z_pos);
 	if (ev->key_nav.keys[KEY_RIGHT])
@@ -51,9 +53,9 @@ void	fdf_events(t_env *env)
 		fdf_apply_matrix(fdf->map, fdf->matrix.rot_x_pos);
 	if (ev->key_nav.keys[KEY_DOWN])
 		fdf_apply_matrix(fdf->map, fdf->matrix.rot_x_neg);
-	if (ev->key_pad.keys[KEY_KP_PLUS])
+	if (ev->key_nav.keys[KEY_PAGEUP])
 		fdf_apply_matrix(fdf->map, fdf->matrix.homo_in);
-	if (ev->key_pad.keys[KEY_KP_MINUS])
+	if (ev->key_nav.keys[KEY_PAGEDOWN])
 		fdf_apply_matrix(fdf->map, fdf->matrix.homo_out);
 	if (event_to_process(ev))
 		fdf->to_draw = 1;
